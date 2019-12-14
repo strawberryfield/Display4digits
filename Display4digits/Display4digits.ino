5cc998f0c937dcb0806e87ad5af4e71e92b4e8ec
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2019-12-14 19:15:38 +0100

    Double counter

--- Display4digits/Display4digits.ino
+++ Display4digits/Display4digits.ino
@@ -12,23 +12,23 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-    MX4_C301.init();
+	MX4_C301.init();
+	MX4_C301.init_timer();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-    int j = 1;
-//    for (int j = 0; j < 13; j++) {
-        MX4_C301.display[0] = j;
-        MX4_C301.display[1] = j+1;
-        MX4_C301.display[2] = j+2;
-        MX4_C301.display[3] = j+3;
-        for (int k = 0; k < 5; k++) {
-            MX4_C301.refresh();
-            delay(500);
- //       }
-    }
+	MX4_C301.dots = 0x08;
+	for (int j = 0; j < 100; j++) {
+		MX4_C301.display[0] = j / 10;
+		MX4_C301.display[1] = j % 10;
+		MX4_C301.display[2] = j / 16;
+		MX4_C301.display[3] = j % 16;
 
-
+		delay(500);
+	}
 }
 
+ISR(TIMER1_COMPA_vect) {        // timer compare interrupt service routine
+	MX4_C301.refresh();
+}
